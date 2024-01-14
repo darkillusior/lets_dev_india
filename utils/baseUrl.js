ab2c2c7da1d3c12dde1a6fba3cf83a077646784d
darkillusior <mysteryuniverse12@gmail.com>
2024-01-14 19:44:43 +0530

    baseUrl updated

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-   :"https://lets-dev-india.vercel.app";
+   :"https://lets-dev-india-ivory.vercel.app";
 
 module.exports = baseUrl;
